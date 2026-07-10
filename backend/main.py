b3099cdbe0f9dd613b3e56f53ac95b8244df52c7
jlw63 <juliusw2017@gmail.com>
2026-07-10 15:11:51 +1200

    Add connectionManager with connect, disconnect and broadcast

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 app = FastAPI()
 
 @app.get("/")
 def health():
     return {"status": "ok"}
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.rooms = {}
+    async def connect(self, websocket, room_code):
+        await websocket.accept() #accepts the websocket connection
+        if room_code not in self.rooms: #checks if the room code exists in the rooms dictionary
+            self.rooms[room_code] = []  #initalizes the room code if it does not exist
+        self.rooms[room_code].append(websocket) #append the websocket to the room code
+
+
+    def disconnect(self, websocket, room_code):
+        self.rooms[room_code].remove(websocket) #remove the websocket from the room code
+        if not self.rooms[room_code]: #if the room is empty
+            del self.rooms[room_code]  #delete the room code from the rooms dictionary
+
+        
+
+    async def broadcast(self, room_code, message):
+        for connection in self.rooms[room_code]: #get the room code from the rooms dictionary
+            await connection.send_json(message) #send the message to all connections in the room code
